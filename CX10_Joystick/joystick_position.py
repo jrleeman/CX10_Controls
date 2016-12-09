a59d757f63590ef0274c7b1db3978eae017c09b4
J.R. Leeman <kd5wxb@gmail.com>
2016-12-08 21:03:38 -0500

    Cleanup joystick python script and add comments

--- CX10_Joystick/joystick_position.py
+++ CX10_Joystick/joystick_position.py
@@ -1,82 +1,28 @@
 import pygame
 import serial
 import time
-# Color RGB Tuples
-black = (0, 0, 0)
-white = ( 255, 255, 255)
-
-# This is a simple class that will help us print to the screen
-# It has nothing to do with the joysticks, just outputing the
-# information.
-class TextPrint:
-    def __init__(self):
-        self.reset()
-        self.font = pygame.font.Font(None, 20)
-
-    def prints(self, screen, textString):
-        textBitmap = self.font.render(textString, True, black)
-        screen.blit(textBitmap, [self.x, self.y])
-        self.y += self.line_height
-
-    def reset(self):
-        self.x = 10
-        self.y = 10
-        self.line_height = 15
-
-    def indent(self):
-        self.x += 10
-
-    def unindent(self):
-        self.x -= 10
 
 
+# Initialize pygame
 pygame.init()
-ser = serial.Serial('/dev/cu.usbmodem14111',115200)
-# Set the width and height of the screen [width,height]
-#size = [500, 700]
-#screen = pygame.display.set_mode(size)
 
-#pygame.display.set_caption("CX-10 Controller")
+# This is the serial port for the Arduino. Be sure
+# to change it for your particular setup.
+ser = serial.Serial('/dev/cu.usbmodem14111',115200)
 
 # Loop until the user clicks the close button.
 done = False
 
-# Used to manage how fast the screen updates
-#clock = pygame.time.Clock()
-
-# Initialize the joysticks
-#pygame.joystick.init()
-
-# Get ready to print
-#textPrint = TextPrint()
-
-# Clear the drawing screen
-#screen.fill(white)
-#textPrint.reset()
-
-# Get count of joysticks
-#joystick_count = pygame.joystick.get_count()
-
 # Joystick number to use, change this if you have more than one
 joystick_number = 0
 
+# Make a joystick object and initialize it
 joystick = pygame.joystick.Joystick(joystick_number)
 joystick.init()
-
-# Get the name from the OS for the controller/joystick
-name = joystick.get_name()
-#textPrint.prints(screen, "Joystick name: {}".format(name) )
 
 # Usually axis run in pairs, up/down for one, and left/right for
 # the other.
 axes = joystick.get_numaxes()
-#textPrint.prints(screen, "Number of axes: {}".format(axes) )
-#textPrint.indent()
-
-#axes_2 = [{'Name':'Roll','AxNum':0},
-#        {'Name':'Pitch','AxNum':1},
-#        {'Name':'Yaw','AxNum':2},
-#        {'Name':'Throttle','AxNum':3}]
 
 # Main Loop
 while done==False:
@@ -88,42 +34,31 @@ while done==False:
         if event.type == pygame.QUIT:
             done=True
 
-        # Joystick actions
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-
-
-
-
-    for i in range( axes ):
-        axis = joystick.get_axis( i )
+    # Get the values of the joystick and scale them. In
+    # my setup the values range from -1 to 1. You can switch
+    # the negtive signs if it is reversed on your equipment.
+    # This will output a value of -500 to 500.
+    for i in range(axes):
+        axis = joystick.get_axis(i)
         if i == 0:
-            roll = round((axis*1000)/2)
+            roll = round(axis * 500)
         elif i == 1:
-            pitch = round((axis*-1000)/2)
+            pitch = round(axis * -500)
         elif i == 2:
-            yaw = round((axis*1000)/2)
+            yaw = round(axis * 500)
         elif i == 3:
-            throttle = round((axis*-1000)/2)
+            throttle = round(axis * -500)
         else:
             pass
+
+    # Get the state of the buttons we'll use to simulate the
+    # left and right push buttons of the CX-10 remote.
     left_button = joystick.get_button(2)
     right_button = joystick.get_button(3)
-    #    textPrint.prints(screen, "Axis {} value: {:>6.3f}".format(i, axis) )
-    #textPrint.unindent()
-    #print "%03d,%03d,%03d,%03d,%01d,%01d\n" %(throttle, pitch, roll, yaw, left_button, right_button)
-    #ser.write("%03d,%03d,%03d,%03d\n" %(throttle, pitch, roll, yaw))
+
+    # Send it out the serial port!
     ser.write("%03d,%03d,%03d,%03d,%01d,%01d\n" %(throttle, pitch, roll, yaw, left_button, right_button))
     ser.flush()
 
-    # Go ahead and update the screen with what we've drawn.
-    #pygame.display.flip()
-
-    # Limit to 20 frames per second
-    #clock.tick(10)
-    #time.sleep(0.01)
 # Close the window and quit.
 pygame.quit()
